75bd11a91fe04f36e8e9dc80643b3301fc96c2f1
akomolafe1996 <48469024+akomolafe1996@users.noreply.github.com>
2019-12-13 13:05:07 +0100

    subpage works

--- rosie.js
+++ rosie.js
@@ -1,11 +1,50 @@
-window.addEventListener("DOMContentLoaded", getData);
+window.addEventListener("DOMContentLoaded", init);
 
-function getData() {
-    fetch("https://cosmicstryder.dk/wordpress/wp-json/wp/v2/sticker?_embed")
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+function init() {
+    if (id) {
+        getSingleTattoo();
+    } else {
+        getFrontpageData();
+    }
+}
+
+function getSingleTattoo() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    console.log(id)
+
+    fetch("https://cosmicstryder.dk/wordpress/wp-json/wp/v2/sticker/" + id)
+        .then(res => res.json())
+        .then(showTattoo)
+
+    function showTattoo(tattoo) {
+        console.log(tattoo)
+        document.querySelector(".tattootitle").innerHTML = tattoo.tattoo_title;
+        document.querySelector(".tattooprice").innerHTML = tattoo.tattoo_price;
+        document.querySelector(".tattoodescription").innerHTML = tattoo.tattoo_description;
+
+
+        const img = document.querySelector("img.cover");
+        const imgPath = tattoo.tattoo_image.guid;
+        img.setAttribute("src", imgPath)
+        img.setAttribute("alt", "Image of Temporary Tattoo" + tattoo.title.rendered)
+
+        const image = document.querySelector("img.secondcover");
+        const imagePath = tattoo.tattoo_image.guid;
+        image.setAttribute("src", imagePath)
+        image.setAttribute("alt", "Image of Temporary Tattoo" + tattoo.title.rendered)
+    }
+}
+
+
+function getFrontpageData() {
+    fetch("https://cosmicstryder.dk/wordpress/wp-json/wp/v2/sticker?_embed&per_page=100")
         .then(res => res.json())
         .then(handleData)
 }
-
 
 function handleData(myData) {
     myData.forEach(showPost)
@@ -16,6 +55,9 @@ function showPost(post) {
 
     const template = document.querySelector(".postTemplate").content;
     const postCopy = template.cloneNode(true);
+
+    const a = postCopy.querySelector("a");
+    a.href = "subpage.html?id=" + post.id
 
 
     const img = postCopy.querySelector("img.cover");
